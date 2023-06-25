25363bd84b0c03b2a729fd08d34651d81d8c968a
Petru-Rafael-Rasca <128633442+Petru-Rafael-Rasca@users.noreply.github.com>
2023-06-24 19:23:25 -1200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from art import logo
+from art import vs
+from game_data import data
+import random
 
+print(logo)
+
+print("Compare A: ")
+ 
+print(vs)
+
+print("Against B: ")
+
+input("Who has more followers? Type 'A' or 'B': ")
